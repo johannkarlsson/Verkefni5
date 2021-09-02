11955f3815e7e1cb297b405333e60dc3da651291
freezyblanco <samlokukongurinn@gmail.com>
2021-09-02 16:14:54 +0000

    working prototype

--- sequence.py
+++ sequence.py
@@ -1,1 +1,24 @@
-sequence.py
+# 1,2,3,6,11,20,37,_,_,_,....
+# gera forrit sem generate-ar fyrstu n numbers
+#búa til sequenceið sjalft
+#búa til forritið sem printar linurnar sem matcha input
+a = 1
+b = 2
+c = 3
+d = a+b+c
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+for i in range(0,n):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    a = d+b+c
+    b = c+d+a
+    c = d+a+b
+    d = a+b+c
+   
+
+
+
